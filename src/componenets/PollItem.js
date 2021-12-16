3c4a53b0808fc23070c4f90b00c2b8c87327fce7
parsa shahmaleki <parsampsh@gmail.com>
2021-12-16 12:26:35 +0330

    poll item: show choices

--- src/componenets/PollItem.js
+++ src/componenets/PollItem.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class PollItem extends React.Component {
-    render() { 
+    render() {
+        const isAuth = localStorage.getItem('token') !== null
         const poll = this.props.poll
+        console.log(isAuth)
         return <div>
             <h5>{poll.title}</h5>
             <p>{poll.description}</p>
@@ -11,6 +13,13 @@ class PollItem extends React.Component {
             {poll.is_published ? null : <span className='badge bg-danger'>Unpublished</span>}
             <div>Total votes: {poll.total_votes_count}</div>
             <div>Created by: <Link to={'/user/' + poll.user.id + '/' + poll.user.username}>{poll.user.username}</Link></div>
+            <div>
+                {poll.choices.map((choice, i) => isAuth ? <div>
+                    <input type='radio' name={'poll' + poll.id} key={i} value={choice.id} /> {choice.title}
+                </div> : <div>
+                    <input disabled type='radio' name={'poll' + poll.id} key={i} value={choice.id} /> {choice.title}
+                </div>)}
+            </div>
             <hr />
         </div>
     }
