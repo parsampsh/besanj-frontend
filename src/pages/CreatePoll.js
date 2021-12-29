e57cdce23d7f1fb7488df957cd29985c24ae5bd3
parsa shahmaleki <parsampsh@gmail.com>
2021-12-29 16:27:06 +0330

    create poll: handle the choices

--- src/pages/CreatePoll.js
+++ src/pages/CreatePoll.js
@@ -12,11 +12,17 @@ class CreatePollPage extends React.Component {
             isLoading: false,
             done: false,
             error: null,
+            choices: [],
+            newChoice: '',
         }
 
         this.titleChangeHandler = this.titleChangeHandler.bind(this)
         this.descriptionChangeHandler = this.descriptionChangeHandler.bind(this)
         this.submitHandler = this.submitHandler.bind(this)
+        this.newChoiceChangeHandler = this.newChoiceChangeHandler.bind(this)
+        this.choiceAddHandler = this.choiceAddHandler.bind(this)
+        this.deleteChoiceHandler = this.deleteChoiceHandler.bind(this)
+        this.choiceChangeHandler = this.choiceChangeHandler.bind(this)
     }
 
     titleChangeHandler(event) {
@@ -32,10 +38,20 @@ class CreatePollPage extends React.Component {
 
         this.setState({isLoading: true})
 
+        let choicesAsStr = ''
+        for (let i = 0; i < this.state.choices.length; i++) {
+            choicesAsStr += this.state.choices[i] + '\n'
+        }
+
+        if (choicesAsStr === '') {
+            this.setState({isLoading: false, error: 'You have not created any choice for this poll'})
+            return
+        }
+
         Api.post('polls/create/', {
             title: this.state.title,
             description: this.state.description,
-            choices: 'a', // TODO : handle choices
+            choices: choicesAsStr,
         }).then(res => {
             if (res.status === 201) {
                 this.setState({isLoading: false, error: null, done: true})
@@ -49,6 +65,44 @@ class CreatePollPage extends React.Component {
                 this.setState({isLoading: false, error: String(error)})
             }
         })
+    }
+
+    newChoiceChangeHandler(event) {
+        this.setState({newChoice: event.target.value})
+    }
+
+    choiceAddHandler(event) {
+        let choices = this.state.choices
+        choices.push(this.state.newChoice)
+
+        this.setState({choices: choices, newChoice: ''})
+
+        //document.getElementsByTagName('input')[1].focus() // TODO : use REF instead
+    }
+
+    deleteChoiceHandler(event) {
+        let i = event.target.attributes.itemindex.value * 1
+        let choices = this.state.choices
+        let newChoices = []
+
+        // TODO : use the correct way to pop an item fron array
+        let x = 0
+        while (x < choices.length) {
+            if (x !== i) {
+                newChoices.push(choices[x])
+            }
+            x++
+        }
+
+        this.setState({choices: newChoices})
+    }
+
+    choiceChangeHandler(event) {
+        let i = event.target.attributes.itemindex.value * 1
+        let choices = this.state.choices
+
+        choices[i] = event.target.value
+        this.setState({choices: choices})
     }
 
     render() {
@@ -69,6 +123,20 @@ class CreatePollPage extends React.Component {
 
                 Description:
                 <textarea maxLength={1000} onChange={this.descriptionChangeHandler} style={{minHeight: 200}} className='form-control' placeholder='Description of the poll' value={this.state.description}></textarea>
+
+                <br />
+
+                <div>
+                    <h5>Choices</h5>
+                    {
+                        this.state.choices.map((item, i) => <div>
+                            <input onChange={this.choiceChangeHandler} itemIndex={i} type='text' value={item} />
+                            <button type='button' className='btn btn-danger' itemIndex={i} onClick={this.deleteChoiceHandler}>Delete</button>
+                        </div>)
+                    }
+                    <input onChange={this.newChoiceChangeHandler} value={this.state.newChoice} type='text' placeholder='Add a choice...' />
+                    <button type='button' onClick={this.choiceAddHandler} className='btn btn-primary'>+</button>
+                </div>
 
                 <br />
 
