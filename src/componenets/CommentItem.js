03eb8f0ed2932b115e525318bd0476aef523b8bd
parsa shahmaleki <parsampsh@gmail.com>
2022-01-09 21:30:44 +0330

    comments: show user username on the top of the comment

--- src/componenets/CommentItem.js
+++ src/componenets/CommentItem.js
@@ -9,14 +9,15 @@ class CommentItem extends React.Component {
     render() {
         let replies = ''
         if (this.props.comment.replies.length > 0) {
-            replies = <div className='m-1 p-1'>
+            replies = <div className='m-1 p-1 comment-item-replies'>
                 Replies:
 
                 <CommentsList comments={this.props.comment.replies} />
             </div>
         }
-        return <div className='bg-dark text-light m-2 p-2'>
-            {this.props.comment.text}
+        return <div className='bg-dark text-light m-2 p-2 comment-item'>
+            <div className='comment-item-user'>{this.props.comment.user.username}:</div>
+            <div style={{clear: 'both'}}>{this.props.comment.text}</div>
 
             {replies}
         </div>
